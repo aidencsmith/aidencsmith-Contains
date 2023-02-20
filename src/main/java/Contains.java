a75e75f9c6679464778f0978cc9471f2eb1eb802
aidencsmith <acsmith7643@gmail.com>
2023-02-20 21:51:56 +0000

    contains test commit

--- src/main/java/Contains.java
+++ src/main/java/Contains.java
@@ -11,6 +11,14 @@ public class Contains {
      * @return true if arr contains target, false otherwise.
      */
     public boolean arrayContains(int[] arr, int target){
-        return false;
+
+        boolean val = false;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                val = true;
+            }
+        }
+        return val;
     }
 }
